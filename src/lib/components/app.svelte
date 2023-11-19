500705a6c86c8eb082ec992d6be278c8fbc93ec8
adil mae <itsadilhadid@gmail.com>
2023-11-20 00:59:38 +0100

    i will convert this project to svelte 4 because svelte 5 is still long to come

--- src/lib/components/app.svelte
+++ src/lib/components/app.svelte
@@ -40,37 +40,37 @@
 </script>
 
 <div
-	class="bg-black text-xl font-mono text-gray-200 h-screen flex flex-col justify-center items-center"
+	class="bg-black text-xl font-mono text-gray-200 flex flex-col justify-center items-center min-h-screen"
 >
 	<input
 		placeholder="Add Todo"
 		onkeypress={(e) => e.key === 'Enter' && add()}
 		bind:value={text}
-		class="transition outline-none focus:ring focus:ring-gray-200 focus:ring-offset-4 focus:ring-offset-black bg-black text-white p-3 rounded-2xl mb-5 w-1/3"
+		class="transition outline-none focus:ring focus:ring-gray-200 focus:ring-offset-4 focus:ring-offset-black bg-black text-white p-3 rounded-2xl my-5 w-1/3"
 	/>
 
 	<ul class="space-y-3 flex flex-col w-1/3">
 		{#each todos as todo, idx}
 			<li
-				on:focus={() => (isFocus === false ? (isFocus = idx) : (isFocus = false))}
-				class="border flex p-4 rounded-2xl border-gray-600 w-full justify-between items-center {isFocus ===
+				onclick={() => (isFocus === false ? (isFocus = idx) : (isFocus = false))}
+				class="transition flex p-4 rounded-2xl border-gray-600 w-full justify-between items-center {isFocus ===
 					idx && 'ring ring-gray-200 ring-offset-4 ring-offset-black'}"
 			>
 				<input
 					on:input={() => check(idx)}
 					type="checkbox"
 					checked={todo.done}
-					class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-600 focus:ring-2"
+					class="transition w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-600 focus:ring-2"
 				/>
 				<input
 					bind:value={todo.text}
 					on:keypress={(e) => e.key === 'Enter' && edit(idx)}
 					value={todo.text}
-					class="border outline-none bg-black p-4 {todo.done ? 'line-through' : ''}"
+					class="mx-7 grow transition outline-none bg-black p-4 {todo.done ? 'line-through' : ''}"
 				/>
 
 				<button
-					class="text-xl font-extrabold active:border-2 active:border-gray-200 w-8 p-1 rounded rounded-full"
+					class="transition text-xl font-extrabold active:border-2 active:border-gray-200 w-8 p-1 rounded rounded-full"
 					on:click={() => remove(idx)}>X</button
 				>
 			</li>
---
+++ src/lib/components/todo/todo.svelte
@@ -0,0 +1,45 @@
+<script>
+	let { text, todos, todo, idx } = $props();
+
+	let isFocus = $state(false);
+
+	function remove(idx) {
+		todos = todos.filter((item, index) => index !== idx);
+	}
+
+	function check(idx) {
+		let temp = todos;
+		temp[idx] = { ...temp[idx], done: !temp[idx]['done'] };
+		todos = temp;
+	}
+
+	function edit(idx) {
+		let temp = todos;
+		temp[idx] = { ...temp[idx], text: temp[idx]['text'] };
+		todos = temp;
+	}
+</script>
+
+<div
+	onclick={() => (isFocus === false ? (isFocus = idx) : (isFocus = false))}
+	class="border flex p-4 rounded-2xl border-gray-600 w-full justify-between items-center {isFocus ===
+		idx && 'ring ring-gray-200 ring-offset-4 ring-offset-black'}"
+>
+	<input
+		on:input={() => check(idx)}
+		type="checkbox"
+		checked={todo.done}
+		class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-600 focus:ring-2"
+	/>
+	<input
+		bind:value={todo.text}
+		on:keypress={(e) => e.key === 'Enter' && edit(idx)}
+		value={todo.text}
+		class=" outline-none bg-gray-500 p-4 {todo.done ? 'line-through' : ''}"
+	/>
+
+	<button
+		class="text-xl font-extrabold active:border-2 active:border-gray-200 w-8 p-1 rounded rounded-full"
+		on:click={() => remove(idx)}>X</button
+	>
+</div>
